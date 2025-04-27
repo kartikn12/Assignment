87cefe9a3e66d909e31310fe0eb56386f373980f
Kartik Nagpurkar <155724189+kartikn12@users.noreply.github.com>
2025-04-27 17:41:20 +0530

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -1,92 +1,76 @@
 #include<stdio.h>
 
-addition()
-{
-	int a,b;
-	printf("\nEnter First Number: ");
-	scanf("%d",&a);
-	printf("Enter Second Number:");
-	scanf("%d",&b);
-	printf("\nYour addition is %d\n",a+b);
+// Function for addition
+void addition() {
+    int a, b;
+    printf("\nEnter First Number: ");
+    scanf("%d", &a);
+    printf("Enter Second Number: ");
+    scanf("%d", &b);
+    printf("\nYour addition is %d\n", a + b);
 }
 
-subtraction()
-{
-	int a,b;
-	printf("\nEnter First Number: ");
-	scanf("%d",&a);
-	printf("Enter Second Number:");
-	scanf("%d",&b);
-	printf("Your subtraction is %d\n",a-b);
+// Function for subtraction
+void subtraction() {
+    int a, b;
+    printf("\nEnter First Number: ");
+    scanf("%d", &a);
+    printf("Enter Second Number: ");
+    scanf("%d", &b);
+    printf("Your subtraction is %d\n", a - b);
 }
 
-multipication()
-{
-	int a,b;
-	printf("\nEnter First Number: ");
-	scanf("%d",&a);
-	printf("Enter Second Number:");
-	scanf("%d",&b);
-	printf("Your multipication is %d\n",a*b);
+// Function for multiplication
+void multiplication() {
+    int a, b;
+    printf("\nEnter First Number: ");
+    scanf("%d", &a);
+    printf("Enter Second Number: ");
+    scanf("%d", &b);
+    printf("Your multiplication is %d\n", a * b);
 }
 
-divison()
-{
-	int a,b;
-	printf("\nEnter First Number: ");
-	scanf("%d",&a);
-	printf("Enter Second Number:");
-	scanf("%d",&b);
-	printf("Your divison is %d\n",a/b);
+// Function for division
+void division() {
+    int a, b;
+    printf("\nEnter First Number: ");
+    scanf("%d", &a);
+    printf("Enter Second Number: ");
+    scanf("%d", &b);
+    printf("Your division is %d\n", a / b);
 }
 
- main()
-{
-	while(1)
-	{
-		int ch;
-		printf("---------------MENU----------------\n\n");
-		printf("1.Addition \n");
-		printf("2.Subtraction \n");
-		printf("3.Multipication \n");
-		printf("4.Divison \n");
-		printf("5.Exit\n");
-		printf("\nEnter your Choice :");
-		scanf("%d",&ch);
-		
-		
-		
-		if(ch==1)
-		{
-			
-		addition();
-		break;
-		}
-	
-		else if(ch==2)
-		{	
-			subtraction();
-			break;
-		}
-		else if(ch==3)
-		{
-			multipication();
-			break;
-		}
-		else if(ch==4)
-		{
-			
-			divison();
-			break;
-		}
-		else if(ch==5)
-		{
-			printf("ThankYou!!\n");
-		}
-		else{
-			printf("Invalid Choice Enter A valid Choice\n");
-			
-		}
-	}
+// Main function
+ main() {
+    while(1) {
+        int ch;
+        
+        // Displaying the menu
+        printf("--------------- MENU ----------------\n\n");
+        printf("1. Addition\n");
+        printf("2. Subtraction\n");
+        printf("3. Multiplication\n");
+        printf("4. Division\n");
+        printf("5. Exit\n");
+        printf("\nEnter your Choice: ");
+        scanf("%d", &ch);
+        
+        // Handling user's choice
+        if (ch == 1) {
+            addition();
+        } else if (ch == 2) {
+            subtraction();
+        } else if (ch == 3) {
+            multiplication();
+        } else if (ch == 4) {
+            division();
+        } else if (ch == 5) {
+            printf("\nThank you!!\n");
+            break; // Exit the loop and end the program
+        } else {
+            printf("\nInvalid Choice. Please enter a valid choice.\n");
+        }
+    }
 
+    
 }
